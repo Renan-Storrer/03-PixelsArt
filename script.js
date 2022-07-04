10e8109c31095e8a0b0a003ddbf67e341711cd88
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 15:25:24 -0300

    mais erros de lint REQUISITO 2 E 3

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let divColor = document.querySelectorAll('.color');
+const divColor = document.querySelectorAll('.color');
 // console.log(divColor);
-let black = divColor[0];
+const black = divColor[0];
 black.style.background = 'black';
 // console.log(black);
-let purple = divColor[1];
+const purple = divColor[1];
 purple.style.background = 'purple';
-let yellow = divColor[2];
+const yellow = divColor[2];
 yellow.style.background = 'yellow'
-let blue = divColor[3];
-blue.style.background = 'rgb(0,217,255)'
-
-
+const blue = divColor[3];
+blue.style.background = 'rgb(0,217,255)';
