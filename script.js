5a9ae1803db371f1dcd2df0a379c8f466b2ff5f9
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 19:03:34 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const divColor = document.querySelectorAll('.color');
-console.log(divColor);
+// console.log(divColor);
 const black = divColor[0];
 black.style.background = 'black';
 // console.log(black);
@@ -20,12 +20,12 @@ for (let index = 0; index < 25; index += 1) {
 
 function selecionaCor(evento) {
     let cores = document.querySelectorAll('.color')
-    console.log(cores);
+    // console.log(cores);
     for (let index = 0; index < cores.length; index += 1) {
         if (cores[index].className = 'color selected') {
             cores[index].classList.remove('selected')
         } else {
-            cores[index];classList.add('selected')
+            cores[index]; classList.add('selected')
         }
     }
     evento.target.classList.add('selected');
@@ -35,3 +35,18 @@ black.addEventListener('click', selecionaCor);
 purple.addEventListener('click', selecionaCor);
 yellow.addEventListener('click', selecionaCor);
 blue.addEventListener('click', selecionaCor);
+
+function pintaPixel(evento) {
+    let corSelecionada = document.querySelector('.selected');
+    let pixelSelecionado = evento.target;
+    pixelSelecionado.style.backgroundColor = corSelecionada.style.backgroundColor
+    //getComputedStyle
+    // console.log(pixelSelecionado.style);
+    // console.log(corSelecionada.style.backgroundColor);
+}
+
+let divs = document.querySelectorAll('.pixel')
+console.log(divs);
+for (let i = 0; i < divs.length; i += 1) {
+    divs[i].addEventListener('click', pintaPixel);
+}
