3e021dfa1a95d3a50e673a050134ad35f39ba959
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 15:18:37 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,13 @@
+let divColor = document.querySelectorAll('.color');
+// console.log(divColor);
+let black = divColor[0];
+black.style.background = 'black';
+// console.log(black);
+let purple = divColor[1];
+purple.style.background = 'purple';
+let yellow = divColor[2];
+yellow.style.background = 'yellow'
+let blue = divColor[3];
+blue.style.background = 'rgb(0,217,255)'
+
+
