217d7027ba95bf1a21d128c0f5edf7ceba1a6c7f
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 16:46:06 -0300

    atualizando

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const divColor = document.querySelectorAll('.color');
-console.log(divColor);
+// console.log(divColor);
 const black = divColor[0];
 black.style.background = 'black';
 // console.log(black);
@@ -11,7 +11,7 @@ const blue = divColor[3];
 blue.style.background = 'rgb(0,217,255)';
 
 const pixelBoard = document.querySelector('#pixel-board')
-// console.log(pixelBoard);
+console.log(pixelBoard);
 for (let index = 0; index < 25; index += 1) {
     const criaPixel = document.createElement('div')
     criaPixel.className = 'pixel'
