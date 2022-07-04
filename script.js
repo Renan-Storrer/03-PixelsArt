c5fd8031fb584cf51889a6eeb2d19790b88da437
Renan Storrer <storrerrenan@gmail.com>
2022-07-04 17:13:35 -0300

    atualizando

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ for (let index = 0; index < 25; index += 1) {
 };
 console.log(pixelBoard);
 
-
 function selecionaCor(evento) {
     console.log(evento.target);
     evento.target.classList.add('selected');
