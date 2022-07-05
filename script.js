d4daf500bd252660bdb7088e0faf40ac4cf7bc06
Renan Storrer <storrerrenan@gmail.com>
2022-07-05 17:59:13 -0300

    erros lint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function corAleatoria () {
+function corAleatoria() {
   const letters = '0123456789ABCDEF';
-  let color = '#'
-  for (let i = 0; i < 6; i++) {
+  let color = '#';
+  for (let i = 0; i < 6; i+= 1) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   
