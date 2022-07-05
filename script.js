b0ad36c77d96093e9361345c6c6855c38e284001
Renan Storrer <storrerrenan@gmail.com>
2022-07-05 14:48:49 -0300

    erros lint js

--- script.js
+++ script.js
@@ -16,16 +16,16 @@ for (let index = 0; index < 25; index += 1) {
   const criaPixel = document.createElement('div');
   criaPixel.className = 'pixel';
   pixelBoard.appendChild(criaPixel);
-};
+}
 
 function selecionaCor(evento) {
-  let cores = document.querySelectorAll('.color');
+  const cores = document.querySelectorAll('.color');
   // console.log(cores);
   for (let index = 0; index < cores.length; index += 1) {
     if (cores[index].className = 'color selected') {
       cores[index].classList.remove('selected');
     } else {
-      cores[index]; classList.add('selected');
+      cores[index] = classList.add('selected');
     }
   }
   evento.target.classList.add('selected');
@@ -37,28 +37,28 @@ yellow.addEventListener('click', selecionaCor);
 blue.addEventListener('click', selecionaCor);
 
 function pintaPixel(evento) {
-  let corSelecionada = document.querySelector('.selected');
-  let pixelSelecionado = evento.target;
+  const corSelecionada = document.querySelector('.selected');
+  const pixelSelecionado = evento.target;
   pixelSelecionado.style.backgroundColor = corSelecionada.style.backgroundColor;
-  //getComputedStyle
+  // getComputedStyle
   // console.log(pixelSelecionado.style);
   // console.log(corSelecionada.style.backgroundColor);
 }
 
-let divs = document.querySelectorAll('.pixel');
+const divs = document.querySelectorAll('.pixel');
 console.log(divs);
 for (let i = 0; i < divs.length; i += 1) {
   divs[i].addEventListener('click', pintaPixel);
 }
 
-function limpaPixel(evento) {
-  let divs = document.querySelectorAll('.pixel');
-  console.log(divs);
-  for (let i = 0; i < divs.length; i += 1){
-    divs[i].style.backgroundColor = 'white'
-    console.log(divs[i].style.backgroundColor);
+function limpaPixel() {
+  const pixeis = document.querySelectorAll('.pixel');
+  console.log(pixeis);
+  for (let i = 0; i < pixeis.length; i += 1) {
+    pixeis[i].style.backgroundColor = 'white';
+    console.log(pixeis[i].style.backgroundColor);
   }
 }
 
-let button = document.querySelector('button');
+const button = document.querySelector('button');
 button.addEventListener('click', limpaPixel);
